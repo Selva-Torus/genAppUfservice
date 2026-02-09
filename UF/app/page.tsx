9ab1fc1ab94eb15c39c2b7930ae6e8a5c9819949
peerm786 <peerm@torus.tech>
2026-02-09 15:04:06 +0530

    fix error

--- UF/app/page.tsx
+++ UF/app/page.tsx
@@ -1,6 +1,6 @@
 
 'use client'
-import LoginForm from './components/LoginForm';
+import LoginForm from '@/app/components/loginForm';
 import { AxiosService } from './components/axiosService';
 import { deleteAllCookies, deleteCookie, getCookie } from './components/cookieMgment';
 import { useEffect } from 'react';
